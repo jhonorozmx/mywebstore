ac93f58faa74c1a8873744a70eb929a38debf52d
estcar08 <Carlos.Estrella@theksquaregroup.com>
2021-11-29 23:29:28 -0600

    first commit PWA base

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import store from "./app/store";
+import * as serviceworker from './serviceworker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -22,3 +23,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceworker.register();
